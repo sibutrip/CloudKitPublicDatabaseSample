fc822ac444deaa1af7b00d7add53c692659afe7a
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-01-30 09:49:58 -0500

    event type

---
+++ CloudKitPublicDatabaseSample/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  CloudKitPublicDatabaseSample
+//
+//  Created by Cory Tripathy on 1/30/24.
+//
+
+import CoreLocation
+
+struct Event {
+    let title: String
+    let venue: String
+    let description: String
+    let date: Date
+}
