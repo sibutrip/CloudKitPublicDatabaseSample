612186f5f92c6ea3eeb05d00856db143ae323e76
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-01-30 09:50:11 -0500

    ck service function stubs

--- CloudKitPublicDatabaseSample/CloudKitService.swift
+++ CloudKitPublicDatabaseSample/CloudKitService.swift
@@ -10,4 +10,8 @@ import CloudKit
 class CloudKitService {
     let container = CKContainer(identifier: "iCloud.com.CoryTripathy.CloudKitShare")
     lazy var database = container.publicCloudDatabase
+    public func saveEvent(_ event: Event) async throws { }
+    public func fetchEvents() -> [Event] async throws { }
+    public func updateEvent(_ event: Event) async throws { }
+    public func deleteEvent(_ event: Event) async throws { }
 }
