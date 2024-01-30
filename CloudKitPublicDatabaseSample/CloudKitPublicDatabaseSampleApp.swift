4fe1651f25522129479344f456769f412518834f
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2024-01-30 09:37:13 -0500

    Initial Commit

---
+++ CloudKitPublicDatabaseSample/CloudKitPublicDatabaseSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitPublicDatabaseSampleApp.swift
+//  CloudKitPublicDatabaseSample
+//
+//  Created by Cory Tripathy on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitPublicDatabaseSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
